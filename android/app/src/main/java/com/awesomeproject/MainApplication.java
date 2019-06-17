3c1db418f9f03ad7104bcb2e6e7f86c6c252f465
Mukund <mukundsharma@outlook.com>
2019-06-17 20:40:28 +0530

    Integrated test ad

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import cl.json.RNSharePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new SplashScreenReactPackage(),
             new RNSharePackage(),
             new LinearGradientPackage(),
--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -8,109 +8,117 @@ import { ENTRIES1 } from './static/entries';
 import styles, { colors } from './styles/index.style';
 import { itemWidth, sliderWidth } from './styles/SliderEntry.style';
 import SplashScreen from 'react-native-splash-screen';
+import {
+  AdMobBanner,
+} from 'react-native-admob';
 
 const IS_ANDROID = Platform.OS === 'android';
 const SLIDER_1_FIRST_ITEM = 1;
 
 export default class MainScreen extends Component {
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            slider1ActiveSlide: SLIDER_1_FIRST_ITEM
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      slider1ActiveSlide: SLIDER_1_FIRST_ITEM
+    };
+  }
 
-    static navigationOptions = {
-        header: null
-      };
+  static navigationOptions = {
+    header: null
+  };
 
-    _renderItemWithParallax ({item, index}, parallaxProps) {
-        return (
-            <SliderEntry
-              data={item}
-              even={(index + 1) % 2 === 0}
-              parallax={true}
-              parallaxProps={parallaxProps}
-            />
-        );
-    }
+  _renderItemWithParallax({ item, index }, parallaxProps) {
+    return (
+      <SliderEntry
+        data={item}
+        even={(index + 1) % 2 === 0}
+        parallax={true}
+        parallaxProps={parallaxProps}
+      />
+    );
+  }
 
-    mainExample (number, title) {
-        const { slider1ActiveSlide } = this.state;
+  mainExample(number, title) {
+    const { slider1ActiveSlide } = this.state;
 
-        return (
-            <View style={styles.exampleContainer}>
-                <Text style={styles.title}>{number}</Text>
-                <Text style={styles.subtitle}>{title}</Text>
-                <Carousel
-                  ref={c => this._slider1Ref = c}
-                  data={ENTRIES1}
-                  renderItem={this._renderItemWithParallax}
-                  sliderWidth={sliderWidth}
-                  itemWidth={itemWidth}
-                  hasParallaxImages={true}
-                  firstItem={SLIDER_1_FIRST_ITEM}
-                  inactiveSlideScale={0.94}
-                  inactiveSlideOpacity={0.7}
-                  containerCustomStyle={styles.slider}
-                  contentContainerCustomStyle={styles.sliderContentContainer}
-                  loop={true}
-                  loopClonesPerSide={2}
-                  onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index }) }
-                />
-                <Pagination
-                  dotsLength={ENTRIES1.length}
-                  activeDotIndex={slider1ActiveSlide}
-                  containerStyle={styles.paginationContainer}
-                  dotColor={'rgba(255, 255, 255, 0.92)'}
-                  dotStyle={styles.paginationDot}
-                  inactiveDotColor={colors.black}
-                  inactiveDotOpacity={0.4}
-                  inactiveDotScale={0.6}
-                  carouselRef={this._slider1Ref}
-                  tappableDots={!!this._slider1Ref}
-                />
-            </View>
-        );
-    }
+    return (
+      <View style={styles.exampleContainer}>
+        <Text style={styles.title}>{number}</Text>
+        <Text style={styles.subtitle}>{title}</Text>
+        <Carousel
+          ref={c => this._slider1Ref = c}
+          data={ENTRIES1}
+          renderItem={this._renderItemWithParallax}
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+          hasParallaxImages={true}
+          firstItem={SLIDER_1_FIRST_ITEM}
+          inactiveSlideScale={0.94}
+          inactiveSlideOpacity={0.7}
+          containerCustomStyle={styles.slider}
+          contentContainerCustomStyle={styles.sliderContentContainer}
+          loop={true}
+          loopClonesPerSide={2}
+          onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index })}
+        />
+        <Pagination
+          dotsLength={ENTRIES1.length}
+          activeDotIndex={slider1ActiveSlide}
+          containerStyle={styles.paginationContainer}
+          dotColor={'rgba(255, 255, 255, 0.92)'}
+          dotStyle={styles.paginationDot}
+          inactiveDotColor={colors.black}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
+          carouselRef={this._slider1Ref}
+          tappableDots={!!this._slider1Ref}
+        />
+      </View>
+    );
+  }
 
 
-    get gradient () {
-        return (
-            <LinearGradient
-              colors={[colors.background1, colors.background2]}
-              startPoint={{ x: 1, y: 0 }}
-              endPoint={{ x: 0, y: 1 }}
-              style={styles.gradient}
-            />
-        );
-    }
+  get gradient() {
+    return (
+      <LinearGradient
+        colors={[colors.background1, colors.background2]}
+        startPoint={{ x: 1, y: 0 }}
+        endPoint={{ x: 0, y: 1 }}
+        style={styles.gradient}
+      />
+    );
+  }
 
-    componentDidMount() {
-      SplashScreen.hide();
-    }
+  componentDidMount() {
+    SplashScreen.hide();
+  }
 
-    render () {
-        const example1 = this.mainExample('Daily Readings', 'Let the tarot cards guide you daily on Love, Career, Health and Family');
+  render() {
+    const example1 = this.mainExample('Daily Readings', 'Let the tarot cards guide you daily on Love, Career, Health and Family');
 
-        return (
-                <View style={styles.container}>
-                    <StatusBar
-                      translucent={true}
-                      backgroundColor={'rgba(0, 0, 0, 0.3)'}
-                      barStyle={'light-content'}
-                    />
-                    { this.gradient }
-                    <ScrollView
-                      style={styles.scrollview}
-                      scrollEventThrottle={200}
-                      contentContainerStyle={{flexGrow : 1, justifyContent : 'space-evenly'}}
-                      directionalLockEnabled={true}
-                    >
-                        { example1 }
-                    </ScrollView>
-                </View>
-        );
-    }
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          translucent={true}
+          backgroundColor={'rgba(0, 0, 0, 0.3)'}
+          barStyle={'light-content'}
+        />
+        {this.gradient}
+        <ScrollView
+          style={styles.scrollview}
+          scrollEventThrottle={200}
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-evenly' }}
+          directionalLockEnabled={true}
+        >
+          {example1}
+          <AdMobBanner
+            adSize="banner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111"
+            onAdFailedToLoad={error => alert(error)}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
 }
