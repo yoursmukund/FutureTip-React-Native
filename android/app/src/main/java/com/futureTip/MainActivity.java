6699065ba537ddb161bc1b0beb832680fa246d2a
Mukund <mukundsharma@outlook.com>
2019-08-23 07:38:32 +0530

    Updated app name changes

--- android/app/src/main/java/com/futureTip/MainActivity.java
+++ android/app/src/main/java/com/futureTip/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.futureTip;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/futureTip/MainApplication.java
+++ android/app/src/main/java/com/futureTip/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.futureTip;
 
 import android.app.Application;
 
